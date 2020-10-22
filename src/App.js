97b0b0e7533b8247ae8be6926f82d5eeb5c281df
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-10-22 22:54:17 +0100

    can fetch data from glitch API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
+import Quotes from './Quotes';
+import QuoteCard from './QuoteCard';
 
 function App() {
+
+const[fetchQuotes, setFetchQuotes]= useState([]);
+
+useEffect(()=>{
+  fetch('https://ekipkalir-quote-server.glitch.me/quotes')
+  .then(response => response.json())
+  .then(data => fetchQuotes(data));
+
+},[]);
+
   return (
     <div className="App">
-
+  {fetchQuotes.map(q => (
+    <QuoteCard key={q.quote} quote={q.quote} author={q.author}/>
+  ))}
     </div>
   );
 }
