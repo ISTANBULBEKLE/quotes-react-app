5447c193aac24e7d63d5dc6ce53a0f1a910c92a8
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-10-31 02:23:50 +0000

    buttonadded

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React, {useState,useEffect} from 'react';
 import './App.css';
-// import Quotes from './Quotes';
-import QuoteCard from './QuoteCard';
 
 function App() {
 
 const[fetchQuotes, setFetchQuotes]= useState([]);
 
-function setQuotes (){
+function handleQuotes (){
+  console.log('hello');
   setFetchQuotes(fetchQuotes);
 }
 
 useEffect(()=>{
-  fetch('https://ekipkalir-quote-server.glitch.me/quotes')
+  fetch('https://cyf-ekipkalir-quotes-id.glitch.me/quotes/random')
   .then(response => response.json())
-  .then(data => setFetchQuotes(data))
+  .then(data => console.log(setFetchQuotes(data)))
   .catch(error => console.log(error));
 
 },[]);
 
   return (
-    <div className="App">
-  {fetchQuotes.map(q => (
-    <QuoteCard key={q.quote} quote={q.quote} author={q.author}/>
-  ))}
-    </div>
+  
+        <div className='card'>
+          <div className='card-div'>
+              <h2 className='card-quote'>{fetchQuotes.quote}</h2>
+              <p className='quote-author'>{fetchQuotes.author}</p>
+          </div>
+          <button onClick={handleQuotes} className="btn btn-lg btn-primary btn-block message message-button" type="submit">Click for a Quote</button>
+        </div>
   );
 }
 
